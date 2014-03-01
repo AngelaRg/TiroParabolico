0508916f4978c11e567c3a4a976bdc87a7bb9836
Angela Romo <angela_9412@hotmail.com>
2014-02-28 19:02:02 -0600

    s sonido, cambio de barquito, corazoncitos, pantallas instrucciones creditos pausa, ajuste de sleep y movimiento barco

--- src/tiroparabolico/Principal.java
+++ src/tiroparabolico/Principal.java
@@ -32,8 +32,10 @@ public class Principal extends JFrame implements Runnable, KeyListener, MouseLis
     private SoundClip sad;    //Objeto AudioClip 
     private Image dbImage;	// Imagen a proyectar	
     private Graphics dbg;	// Objeto grafico
-    private Color c; //para color de strings
     private Image fondo;	//Imagen de fondo del JFrame
+    private Image pantallaInstr; //Imagen de instrucciones
+    private Image pantallaPau; //Imagen de pausa
+    private Image pantallaCreditos; //Imagen de creditos
     private boolean pausa; // bandera para manejar la pausa
     private boolean instrucciones; //bandera para manejar despliegue de instrucciones
     private Nube nubecita; // nube del juego
@@ -42,6 +44,7 @@ public class Principal extends JFrame implements Runnable, KeyListener, MouseLis
     private boolean click; //identificar click
     private int clickX; //coordenada de click en X
     private int clickY; //coordenada de click en Y
+    private boolean sonidoActivado; //controla sonido
     private boolean limitesBarquitoIzquierda; // bandera para delimitar que el barquito no se salga por el lado izquierdo del jFrame
     private boolean limitesBarquitoDerecha; // bandera para delimitar que no se salga por el lado derecho 
     private double velocidadInicial;
@@ -52,7 +55,11 @@ public class Principal extends JFrame implements Runnable, KeyListener, MouseLis
     private int score;
     private int vidas;
     private int contPerdidas;
-    private double h, R; //altura maxima h y alcance maximo R (Formulas físicas de tiro parabolico) 
+    private Image heart1; // corazones
+    private Image heart2; // corazones
+    private Image heart3; // corazones
+    private Image heart4; // corazones
+    private Image heart5; // corazones
 
     //Constructor (aqui se pone todo lo del init)
     public Principal() {
@@ -65,9 +72,18 @@ public class Principal extends JFrame implements Runnable, KeyListener, MouseLis
         //SONIDOS
         happy = new SoundClip("Sounds/diamond.wav");
         sad = new SoundClip("Sounds/Explosion.wav");
+        sonidoActivado = true;
         //FONDO
-        //Carga la imagen de fondo
+        //Carga la imagen de fondo, las pantallas y los corazones de vidas
         fondo = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("fondo/imagenmar.jpg"));
+        pantallaInstr = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("pantallas/Instrucciones.jpg"));
+        pantallaPau = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("pantallas/Pausa.jpg"));
+        pantallaCreditos = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("pantallas/Creditos.jpg"));
+        heart1 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("pantallas/heartif.png"));
+        heart2 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("pantallas/heartif.png"));
+        heart3 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("pantallas/heartif.png"));
+        heart4 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("pantallas/heartif.png"));
+        heart5 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("pantallas/heartif.png"));
         //Pausa y clicks
         pausa = false;
         instrucciones = false;
@@ -82,7 +98,7 @@ public class Principal extends JFrame implements Runnable, KeyListener, MouseLis
         nubecita = new Nube(0, getHeight() / 2);
 
         barquito = new Barco(getWidth() / 2, getHeight());
-        barquito.setPosY(getHeight() - 2 * barquito.getAlto() + 55); //reposicionar en la parte de abajo del applet
+        barquito.setPosY(getHeight() - 2 * barquito.getAlto()); //reposicionar en la parte de abajo del applet
         rayito = new Rayo(0, nubecita.getPosY() + 18);
         limitesBarquitoIzquierda = false;
         limitesBarquitoDerecha = false;
@@ -117,7 +133,7 @@ public class Principal extends JFrame implements Runnable, KeyListener, MouseLis
             repaint(); // Se actualiza el <code>Applet</code> repintando el contenido.
             try {
                 // El thread se duerme.
-                Thread.sleep(200);
+                Thread.sleep(150);
             } catch (InterruptedException ex) {
                 System.out.println("Error en " + ex.toString());
             }
@@ -147,18 +163,18 @@ public class Principal extends JFrame implements Runnable, KeyListener, MouseLis
             case 1: // se mueve a la izquierda
                 barquito.actualiza(tiempoActual);
                 if (!limitesBarquitoIzquierda) {
-                    barquito.setPosX(barquito.getPosX() - 15);
+                    barquito.setPosX(barquito.getPosX() - 30);
                 } else {
-                    barquito.setPosX(barquito.getPosX() + 15); // si esta chocando movemos 10 unidades a la derecha al barquito
+                    barquito.setPosX(barquito.getPosX() + 15); // si esta chocando movemos 15 unidades a la derecha al barquito
                 }
 
                 break;
             case 2: // se mueve a la derecha
                 barquito.actualiza(tiempoActual);
                 if (!limitesBarquitoDerecha) {
-                    barquito.setPosX(barquito.getPosX() + 15);
+                    barquito.setPosX(barquito.getPosX() + 30);
                 } else {
-                    barquito.setPosX(barquito.getPosX() - 15); // si esta chocando, movemos 10 unidades a la izquierda al barquito
+                    barquito.setPosX(barquito.getPosX() - 15); // si esta chocando, movemos 15 unidades a la izquierda al barquito
                 }
                 break;
 
@@ -180,7 +196,9 @@ public class Principal extends JFrame implements Runnable, KeyListener, MouseLis
         }
 
         if (rayito.intersecta(barquito)) {
-            happy.play(); //tocar sonido
+            if (sonidoActivado) {
+                happy.play(); //tocar sonido
+            }
             click = false; //resetear click
             score += 2; //aumenta 2 por cada rayo atrapado
             //reiniciar valores del rayo para siguiente jugada
@@ -194,7 +212,9 @@ public class Principal extends JFrame implements Runnable, KeyListener, MouseLis
 
         }
         if (rayito.getPosY() > getHeight()) {
-            sad.play(); //tocar sonido
+            if (sonidoActivado) {
+                sad.play(); //tocar sonido
+            }
             click = false; //resetear click
             contPerdidas++; //se agrega una perdida mas           
             //reiniciar valores del rayo para siguiente jugada
@@ -238,15 +258,37 @@ public class Principal extends JFrame implements Runnable, KeyListener, MouseLis
     // aqui va todo lo que iba en el paint
     public void paint1(Graphics g) {
         if (vidas <= 0) {
-            g.drawString("GAME OVER", getWidth() / 2 - 50, getHeight() / 2 - 10);
+            g.drawImage(pantallaCreditos, 0, 0, getSize().width, getSize().height, this);
         } else if (instrucciones) {
-            g.drawString("INSTRUCCIONES", getWidth() / 2 - 60, 100);
+            g.drawImage(pantallaInstr, 0, 0, getSize().width, getSize().height, this);
+        } else if (pausa) {
+            g.drawImage(pantallaPau, 0, 0, getSize().width, getSize().height, this);
         } else {
             //Dibuja la imagen de fondo
             g.drawImage(fondo, 0, 0, getSize().width, getSize().height, this);
             g.drawString("Vidas: " + vidas, getWidth() - 200, 50);
-            if (pausa) {
-                g.drawString("PAUSA", getWidth() / 2 - 10, 200);
+            g.drawString("Score: " + score, getWidth() - 200, 70);
+            //corazoncitos que también indican las vidas
+            if (vidas == 5) {
+                g.drawImage(heart5, getWidth() - 140, 35, this);
+                g.drawImage(heart4, getWidth() - 120, 35, this);
+                g.drawImage(heart3, getWidth() - 100, 35, this);
+                g.drawImage(heart2, getWidth() - 80, 35, this);
+                g.drawImage(heart1, getWidth() - 60, 35, this);
+            } else if (vidas == 4) {
+                g.drawImage(heart4, getWidth() - 120, 35, this);
+                g.drawImage(heart3, getWidth() - 100, 35, this);
+                g.drawImage(heart2, getWidth() - 80, 35, this);
+                g.drawImage(heart1, getWidth() - 60, 35, this);
+            } else if (vidas == 3) {
+                g.drawImage(heart3, getWidth() - 100, 35, this);
+                g.drawImage(heart2, getWidth() - 80, 35, this);
+                g.drawImage(heart1, getWidth() - 60, 35, this);
+            } else if (vidas == 2) {
+                g.drawImage(heart2, getWidth() - 80, 35, this);
+                g.drawImage(heart1, getWidth() - 60, 35, this);
+            } else if (vidas == 1) {
+                g.drawImage(heart1, getWidth() - 60, 35, this);
             }
             if ((nubecita != null) && (barquito != null) && (rayito != null)) {
                 g.drawImage(rayito.getImagenI(), (int) rayito.getPosX(), (int) rayito.getPosY(), this);
@@ -282,9 +324,11 @@ public class Principal extends JFrame implements Runnable, KeyListener, MouseLis
             instrucciones = !instrucciones; //quito o pongo instrucciones
             if (instrucciones) {  // pauso cuando hay instrucciones para evitar que el juego continue mientras aparecen estas
                 pausa = true;
-                //  } else {
-                //      pausa = false;
             }
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_S) {
+            sonidoActivado = !sonidoActivado; //activo o desactivo el sonido
         }
     }
 
